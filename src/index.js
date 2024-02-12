1a237d0e33868df1c64cc8fe0a9acb026378a639
UYulong <1274583083@qq.com>
2024-02-12 15:29:29 +0800

    feat: 加入rem机制

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import '@/assets/reset.min.css'
+import 'lib-flexible'
+
+import './index.less'
+
+// 处理最大宽度
+(function () {
+  const handleMaxWidth = () => {
+    const html = document.documentElement,
+      root = document.getElementById('root'),
+      deviceWidth = html.clientWidth
+
+    root.style.maxWidth = '750px'
+
+    if (deviceWidth >= 750) {
+      html.style.fontSize = '75px'
+    }
+  }
+
+  handleMaxWidth()
+})()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
